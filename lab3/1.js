60295dc6b749a0589974869caef2eef96ac778a4
alexclass98 <alexclass98@mail.ru>
2022-03-13 13:16:58 +0300

    lab3 ready

---
+++ lab3/1.js
@@ -0,0 +1,21 @@
+/**
+ * Напишите функцию capitalize(str),
+ * которая вернет строку str, в которой каждое слово начинается с заглавной буквы.
+ * Примеры:
+ * 'я вижу солнце' -> 'Я Вижу Солнце'
+ * 'я Вижу солнце' -> 'Я Вижу Солнце'
+ */
+function capitalize(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i]==" "){
+            str = str.substr(0, i) + " " + str[i+1].toUpperCase() + str.substr(i + 2);
+        }
+        if(i==0){
+            str = str[i].toUpperCase()+str.substr(i+1);
+        }
+    }
+
+    return str;
+}
+
+module.exports = capitalize;
---
+++ lab3/2.js
@@ -0,0 +1,23 @@
+/**
+ * Напишите функцию getMinMax(str),
+ * на вход в функцию подается строка str
+ * числа в строке выделяются пробелами или знаками препинания
+ * необходимо найти минимальное и максимальное число в строке
+ * вернуть в формате {min: 1, max: 22}
+ * Примеры:
+ * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
+ */
+function getMinMax(str) {
+    let min, max;
+    str = str.split(/,| |]/);
+    min = max = Number(str[0]);
+
+    for(let i = 1; i < str.length; i++){
+        str[i] = Number(str[i]);
+        if((str[i] > max) && (typeof str[i] == "number")) max = str[i];
+        if((str[i] < min) && (typeof str[i] == "number")) min = str[i];
+    }
+    return {'min': min, 'max': max};
+}
+
+module.exports = getMinMax;
