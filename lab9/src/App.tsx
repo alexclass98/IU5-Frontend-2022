0ccbcc11023252301d419d0342f576fb93fb5521
Alex <alexclass98@mail.ru>
2022-05-31 13:32:25 +0300

    laba10 new

--- lab9/src/App.tsx
+++ lab9/src/App.tsx
@@ -12,7 +12,7 @@ export const App = () => {
                 <h2 className="text1">Данная страница поможет тебе найти пользователя github по его логину!</h2>
              </header>
              <Routes>
-                <Route path={process.env.REACT_APP_DEV === "true" ? "/lab9/build" : "/lab9/build"} element={<GetData/>}></Route>
+                <Route path={process.env.REACT_APP_DEV === "true" ? "/lab9/build/index.html" : "/lab9/build/index.html"} element={<GetData/>}></Route>
                 <Route path={process.env.REACT_APP_DEV === "true" ? "/cool" : "/cool"} element={<OwnPage/>}></Route>
             </Routes>
         </div>
